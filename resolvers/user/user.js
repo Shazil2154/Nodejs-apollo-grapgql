cc63fd2705c7e8f892f08ed3061ae1c45e169744
bilalashraf <bilalashraf1710@gmail.com>
2022-04-08 12:54:17 +0500

    added more queries and mutations for user

--- resolvers/user/user.js
+++ resolvers/user/user.js
@@ -1,7 +1,22 @@
+const { UserInputError } = require("apollo-server-express");
 const User = require("../../models/userModel");
 
 module.exports = {
-  Query: {},
+  Query: {
+    getAllUsers: async () => await User.find(),
+    getUser: async (_, { id }) => await User.findById(id),
+    userLogin: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new UserInputError("User not found");
+      }
+      const isValidPassword = await user.matchPassword(password);
+      if (!isValidPassword) {
+        throw new UserInputError("Invalid password");
+      }
+      return user;
+    },
+  },
   Mutation: {
     createUser: async (_, { user }) =>
       await User.create({
@@ -10,5 +25,7 @@ module.exports = {
         email: user.email,
         password: user.password,
       }),
+    updateUser: async (_, { id, user }) => await User.findByIdAndUpdate(id, user, { new: true }),
+    deleteUser: async (_, { id }) => await User.findByIdAndDelete(id),
   },
 };
--- typeDefs/user/user.js
+++ typeDefs/user/user.js
@@ -14,7 +14,22 @@ module.exports = gql`
     email: String!
     password: String!
   }
+
+  input UserUpdateInput {
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+
+  extend type Query {
+    getAllUsers: [User]
+    getUser(id: ID!): User
+    userLogin(email: String!, password: String!): User
+  }
   extend type Mutation {
     createUser(user: UserInput): User
+    updateUser(id: ID!, user: UserUpdateInput): User
+    deleteUser(id: ID!): User
   }
 `;
